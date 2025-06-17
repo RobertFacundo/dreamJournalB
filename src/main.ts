f9ec8a78da86bb18d8cffd421a0cc40db3f5ed5a
Facundo Robert <robert.facundo@hotmail.com>
2025-06-17 10:53:07 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://dream-journal-f.vercel.app'],
     credentials: true
   })
   await app.listen(process.env.PORT ?? 3000);
